43a72f5227796b8c4f72000863a1a16d4c58ff94
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2026-07-30 20:52:03 +0530

    Create Important Sequences.c for escape sequences
    Add a C program demonstrating various escape sequences.

---
+++ Escape Sequences/Important Sequences.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+  printf("hello\nworld");
+  printf("\nname\tage");
+  printf("\nHi\\ how are you?");
+  printf("\nabc\bde");
+  printf("\n12345\rab");
+  return 0;
+}
+
