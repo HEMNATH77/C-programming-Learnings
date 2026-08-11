254e5e34f4da5e81fb3600befb3b26633c322554
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2026-08-11 18:04:59 +0530

    Add files via upload

---
+++ Basic Refresher/Size_of_datatypes.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ 
+    printf("int  => %zu\n", sizeof(int));  // printing size of int
+    printf("char => %zu\n", sizeof(char));  // printing size of char
+    printf("float => %zu\n", sizeof(float));  // printing size of float
+    printf("double => %zu\n", sizeof(double));  // printing size of double
+    return 0;
+}
---
+++ Basic Refresher/format_specifiers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    // 1. Variable Declaration
+
+    /* int age;
+    char name;
+    float marks;
+    double total;
+    printf("int => %d\n",age);
+    printf("char => %c\n",name);
+    printf("float => %f\n",marks);
+    printf("double => %lf\n",total); */
+    // Prints the Garbage Value of all the variables...
+
+    // 2.Variable Declaration
+
+    int age = 10;
+    char name = 'h';
+    float marks = 73.56;
+    double total = 234.7;
+    printf("int => %d\n",age);
+    printf("char => %c\n",name);
+    printf("float => %f\n",marks);
+    printf("double => %lf\n",total);
+    printf("float => %g\n",marks);
+    return 0; 
+}
