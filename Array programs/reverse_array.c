531c75f6cdf10975b0a724a746c851cc97e80df2
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2026-08-31 21:15:31 +0530

    Add files via upload

---
+++ Array programs/reverse_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Array Elements in reverse\n");
+    for(int i=n-1;i>=0;i--){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
