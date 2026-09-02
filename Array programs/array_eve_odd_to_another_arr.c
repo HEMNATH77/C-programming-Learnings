040ac60071983a07c20b1a106bca8b1a5eb57270
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2026-09-02 19:09:43 +0530

    Create array_eve_odd_to_another_arr.c

---
+++ Array programs/array_eve_odd_to_another_arr.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int odd_arr[n],even_arr[n];
+    
+    for(int i=0;i<n;i++){
+        even_arr[i] = 0;
+        odd_arr[i] = 0;
+    }
+    
+    
+    int eve = 0;
+    int odd = 0;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]%2 ==0){
+            even_arr[eve] = arr[i];
+            eve++;
+        }
+        else{
+            odd_arr[odd] = arr[i];
+            odd++;
+        }
+    }
+    
+    printf("Even array: \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",even_arr[i]);
+    }
+    printf("\n");
+    printf("Odd Array: \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",odd_arr[i]);
+    }
+    
+    return 0;
+}
