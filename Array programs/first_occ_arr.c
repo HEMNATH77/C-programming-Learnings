d6de74c83095d321a1aaca179057dc16baad44c7
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2026-09-03 18:44:21 +0530

    Add files via upload

---
+++ Array programs/first_occ_arr.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int flag = 0;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Array elements: \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    int ele;
+    printf("Enter the element: ");
+    scanf("%d",&ele);
+    
+    for(int i=0;i<n;i++){
+        if(ele == arr[i]){
+            printf("index : %d",i);
+            flag = 1;
+            break;
+        }
+    }
+    printf("\n");
+    if(flag == 1){
+        printf("element found\n");
+    }
+    else{
+        printf("Not found\n");
+    }
+    return 0;
+}
